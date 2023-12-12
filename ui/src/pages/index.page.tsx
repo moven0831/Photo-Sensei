4de5d855ac6410ca13be56a0efe59e9b7ddf7498
moven0831 <moven0831@gmail.com>
2023-12-13 04:12:32 +0800

    feat(index): add wallet

--- ui/src/pages/index.page.tsx
+++ ui/src/pages/index.page.tsx
@@ -4,25 +4,70 @@ import GradientBG from "../components/GradientBG.js";
 import InputFile from "../components/InputFile.tsx";
 import SelectOperation from "../components/SelectOperation.tsx";
 
+
+async function handleSubmit() {
+    const { Mina, PublicKey } = await import('o1js');
+    const { ImageTransform } = await import('../../../contracts/build/src/');
+    // await ImageTransform.compile();
+    try {
+      const zkAppAddress = 'B62qnxE3BZK3ZTzZoxoZZMFvFsyJduYESeyd5g9wnYNk4gBzft4KkNk';
+      const tx = await Mina.transaction(() => {
+        const imageTranformInstance = new ImageTransform(PublicKey.fromBase58(zkAppAddress));
+        // await imageTranformInstance.compile();
+        imageTranformInstance.deploy();
+        imageTranformInstance.update();
+        // imageTranformInstance.grayscale();
+      });
+  
+      await tx.prove();
+  
+      const { hash } = await window.mina.sendTransaction({
+        transaction: tx.toJSON(),
+        feePayer: {
+          fee: '',
+          memo: 'zk',
+        },
+      });
+  
+      console.log(hash);
+    } catch (err) {
+      console.log(err.message);
+    }
+}
+
+
 export default function Home() {
     const [image, setImage] = useState<File | null>(null);
     const [operation, setOperation] = useState<string | null>("grayscale");
     useEffect(() => {
-        // (async () => {
-        //   const { Mina, PublicKey } = await import('o1js');
-        //   const { Add } = await import('../../../contracts/build/src/');
-        //   // Update this to use the address (public key) for your zkApp account.
-        //   // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
-        //   // Berkeley Testnet B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA.
-        //   const zkAppAddress = '';
-        //   // This should be removed once the zkAppAddress is updated.
-        //   if (!zkAppAddress) {
-        //     console.error(
-        //       'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA'
-        //     );
-        //   }
-        //   //const zkApp = new Add(PublicKey.fromBase58(zkAppAddress))
-        // })();
+        (async () => {
+          const { Mina, PublicKey } = await import('o1js');
+          const { ImageTransform } = await import('../../../contracts/build/src/');
+          // Update this to use the address (public key) for your zkApp account.
+          // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
+          // Berkeley Testnet B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA.
+          const zkAppAddress = 'B62qnxE3BZK3ZTzZoxoZZMFvFsyJduYESeyd5g9wnYNk4gBzft4KkNk';
+          // This should be removed once the zkAppAddress is updated.
+          if (!zkAppAddress) {
+            console.error(
+              'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA'
+            );
+          }
+
+          let accounts;
+          
+          try {
+              // Accounts is an array of string Mina addresses.
+              accounts = await window.mina.requestAccounts();
+
+              // Show first 6 and last 4 characters of user's Mina account.
+              const display = `${accounts[0].slice(0, 6)}...${accounts[0].slice(-4)}`;
+            } catch (err) {
+              // If the user has a wallet installed but has not created an account, an
+              // exception will be thrown. Consider showing "not connected" in your UI.
+              console.log(err.message);
+           }
+        })();
     }, []);
 
     return (
@@ -35,9 +80,10 @@ export default function Home() {
             <GradientBG>
                 <form
                     className="flex flex-col m-auto w-fit mt-10"
-                    onSubmit={(e) => {
+                    onSubmit={async (e) => {
                         e.preventDefault();
                         console.log(image, operation);
+                        handleSubmit();
                     }}
                 >
                     <div className="m-auto font-bold text-3xl font-mono text-gray-700">Photo Sensei</div>
